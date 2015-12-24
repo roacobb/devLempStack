1a8b0f7e701df5e2b9ccd85f61748c95a3f970a9
Rodney Cobb <roacobb@gmail.com>
2015-12-24 12:04:18 -0500

    Script refactor

--- server_install.sh
+++ server_install.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get -y update
 sudo apt-get -y install nano git curl
-sudo apt-get install nginx
-sudo apt-get install mysql-server
+sudo apt-get -y install nginx
+sudo apt-get -y install mysql-server
 sudo mysql_install_db
 sudo mysql_secure_installation
-sudo apt-get install php5-fpm php5-mysql
+sudo apt-get -y install php5-fpm php5-mysql
